48d1fe1c2a2259f0e768be47c4a0d635f0a23ca9
phollandf5 <45519540+phollandf5@users.noreply.github.com>
2019-05-06 21:01:37 +0100

    Delete provider.tf

--- HA_via_lb_DO_AS3/provider.tf
+++
@@ -1,8 +0,0 @@
-# Configure the Microsoft Azure Provider, replace Service Principal and Subscription with your own
-provider "azurerm" {
-    subscription_id = "${var.SP["c8bd4483-a1a2-47c4-acc8-4a49fbf180f3"]}"
-    client_id       = "${var.SP["6a3ff318-e574-42b7-94ef-c81141ef5f5c"]}"
-    client_secret   = "${var.SP["oinG0qT_z@BOymz4TP*?6IOUhf79T4.V"]}"
-    tenant_id       = "${var.SP["e569f29e-b098-4cea-b6f0-48fa8532d64a"]}"
-}
-
